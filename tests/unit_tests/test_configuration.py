4788bdcd50399b6bce567f9186a7f2ebb4c4a443
yuguang dang <yuguang.dang@technology1.com>
2025-03-20 15:36:48 +1000

    test: add dummy test and pytest config

--- tests/unit_tests/test_configuration.py
+++ tests/unit_tests/test_configuration.py
@@ -3,3 +3,8 @@ from plus_assistant.configuration import Configuration
 
 def test_configuration_empty() -> None:
     Configuration.from_runnable_config({})
+
+
+def test_dummy() -> None:
+    """A dummy test to get started."""
+    assert True
