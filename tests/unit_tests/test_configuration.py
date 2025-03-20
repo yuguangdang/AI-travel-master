18b0988fd0e7d82f09f6749f579e0af2f3487f31
yuguang dang <yuguang.dang@technology1.com>
2025-03-20 15:45:47 +1000

    test: simplify to dummy tests

--- tests/unit_tests/test_configuration.py
+++ tests/unit_tests/test_configuration.py
@@ -1,10 +1,11 @@
-from plus_assistant.configuration import Configuration
-
-
-def test_configuration_empty() -> None:
-    Configuration.from_runnable_config({})
+"""Test the configuration module."""
 
 
 def test_dummy() -> None:
     """A dummy test to get started."""
     assert True
+
+
+def test_another_dummy() -> None:
+    """Another dummy test."""
+    assert 1 + 1 == 2
